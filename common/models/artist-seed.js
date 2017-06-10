c9080ec2a208cbe9366a8210f5c4f9cdbac7dca4
Prateek Rastogi <p.rastogi@outlook.com>
2017-06-11 04:07:44 +0530

    Genesis

---
+++ common/models/artist-seed.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (artistSeed) {
+
+  /**
+   * Get the top N spotify artists of various spotify genre seeds
+   * @param {Function(Error)} callback
+   */
+
+  artistSeed.putTopSpotifyArtists = function (callback) {
+    // TODO
+    callback(null);
+  };
+
+};
