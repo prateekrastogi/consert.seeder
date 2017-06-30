5532fee0fde34d53cf38e676f62763241fc01414
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-01 00:30:08 +0530

    added application insights

--- server/server.js
+++ server/server.js
@@ -2,32 +2,19 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var Raven = require('raven');
 var app = module.exports = loopback();
+var appInsights = require('applicationinsights');
+
+appInsights.setup('edab88eb-c6f2-484a-8da8-0cb5aed1e145')
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .start();
 
 
 app.start = function () {
-
-// Must configure Raven before doing anything else with it
-  Raven.config('https://50da01e5270b41ab8953665a84ce3318:07c2af5d1e824c41a7e412df1f534616@sentry.io/185334').install();
-
-// The request handler must be the first middleware on the app
-  app.use(Raven.requestHandler());
-
-  app.get('/', function mainHandler(req, res) {
-    throw new Error('Broke!');
-  });
-
-// The error handler must be before any other error middleware
-  app.use(Raven.errorHandler());
-
-// Optional fallthrough error handler
-  app.use(function onError(err, req, res, next) {
-    // The error id is attached to `res.sentry` to be returned
-    // and optionally displayed to the user for support.
-    res.statusCode = 500;
-    res.end(res.sentry + '\n');
-  });
 
   // start the web server
   return app.listen(function () {
