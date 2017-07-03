6a7626c45534d23e050133b17a02b2ea55c3b70d
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-03 07:16:25 +0530

    basic spotify api integration and created a helper module

--- common/models/artist-seed.js
+++ common/models/artist-seed.js
@@ -1,44 +1,23 @@
 'use strict';
 
-var SpotifyWebApi = require('spotify-web-api-node');
+var loginAssist = require('../../lib/loginAssist');
 
-
-module.exports = function (artistSeed) {
-
+module.exports = function(artistSeed) {
   /**
    * Put the top N spotify artists of various spotify genre seeds
    * @param {Function(Error, boolean)} callback
    */
 
-  artistSeed.putTopSpotifyArtists = function (callback) {
-
+  artistSeed.putTopSpotifyArtists = function(callback) {
     var isSuccess = true;
-    var clientId = '5084d28ce6924d7b98884323abc76109',
-      clientSecret = 'd1f1a832f28d41c685eda422249da094';
+    var { authPromise, spotifyApi } = loginAssist.spotifyLogin();
 
-// Create the api object with the credentials
-    var spotifyApi = new SpotifyWebApi({
-      clientId: clientId,
-      clientSecret: clientSecret
+    authPromise.then(function(data) {
+      return spotifyApi.getAvailableGenreSeeds();
+    }).then(function(data) {
+      console.log(data.body['genres']);
     });
-
-// Retrieve an access token.
-    var callkey = spotifyApi.clientCredentialsGrant()
-      .then(function (data) {
-        console.log('The access token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-      }, function (err) {
-        console.log('Something went wrong when retrieving an access token', err);
-      });
-
-    callkey.then(function (data) {
-      console.log(spotifyApi.getAccessToken());
-    });
-
     // TODO
     callback(null, isSuccess);
   };
-
 };
