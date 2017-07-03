e37d7af2d5372e15a3815beb7f3cf48e52af48e3
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-03 08:07:11 +0530

    switched to standard

--- common/models/artist-seed.js
+++ common/models/artist-seed.js
@@ -1,23 +1,22 @@
-'use strict';
+'use strict'
 
-var loginAssist = require('../../lib/loginAssist');
+var loginAssist = require('../../lib/loginAssist')
 
-module.exports = function(artistSeed) {
+module.exports = function (artistSeed) {
   /**
    * Put the top N spotify artists of various spotify genre seeds
    * @param {Function(Error, boolean)} callback
    */
 
-  artistSeed.putTopSpotifyArtists = function(callback) {
-    var isSuccess = true;
-    var {authPromise, spotifyApi} = loginAssist.spotifyLogin();
+  artistSeed.putTopSpotifyArtists = async function (callback) {
+    var isSuccess = true
+    var {authPromise, spotifyApi} = loginAssist.spotifyLogin()
 
-    authPromise.then(function(data) {
-      return spotifyApi.getAvailableGenreSeeds();
-    }).then(function(data) {
-      console.log(data.body['genres']);
-    });
+    await authPromise
+    var data = await spotifyApi.getAvailableGenreSeeds()
+    console.log(data)
+
     // TODO
-    callback(null, isSuccess);
-  };
-};
+    callback(null, isSuccess)
+  }
+}
