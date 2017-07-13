d9caf2664168cca7ed078601962dd910d7d93f91
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-13 05:33:58 +0530

    State Preservation

--- common/models/enriched-artists.js
+++ common/models/enriched-artists.js
@@ -13,21 +13,53 @@ module.exports = function (enrichedArtists) {
     const spotifyApi = Rx.Observable.fromPromise(loginAssist.spotifyLogin())
     const artistList = Rx.Observable.from(sample.sampleData)
 
-    artistList.mergeMap(artist => Rx.Observable.zip(spotifyApi, Rx.Observable.of(artist).pluck('id')))
-      .mergeMap(([spotifyApi, artistId]) => {
+    artistList.concatMap(artist => Rx.Observable.zip(spotifyApi, Rx.Observable.of(artist).pluck('id')))
+      .concatMap(([spotifyApi, artistId]) => {
         const artist = Rx.Observable.fromPromise(spotifyApi.getArtist(artistId)).pluck('body')
 
         const artistTopTracks = Rx.Observable.fromPromise(spotifyApi.getArtistTopTracks(artistId, 'US')).pluck('body', 'tracks')
 
-        const artistRelatedArtist = Rx.Observable.fromPromise(spotifyApi.getArtistRelatedArtists(artistId)).pluck('body', 'artists')
+        const artistRelatedArtists = Rx.Observable.fromPromise(spotifyApi.getArtistRelatedArtists(artistId)).pluck('body', 'artists')
 
-        const artistAlbums = Rx.Observable.fromPromise(spotifyApi.getArtistAlbums(artistId, {limit: 50})).pluck('body', 'items')
+        const artistAlbums = Rx.Observable.fromPromise(spotifyApi.getArtistAlbums(artistId, {
+          market: 'US',
+          limit: 50
+        })).pluck('body', 'items')
+
+        const album = artistAlbums.concatMap((albums) => {
+          return Rx.Observable.from(albums)
+        })
+
+        const albumTracks = album.pluck('id').concatMap((id) => {
+          return Rx.Observable.fromPromise(spotifyApi.getAlbumTracks(id, {limit: 50}))
+        }).pluck('body', 'items')
+
+        const albumTrack = albumTracks.concatMap((tracks) => {
+          return Rx.Observable.from(tracks)
+        })
+
+        const timeDelayedAlbumTrackId = Rx.Observable.zip(albumTrack.pluck('id'), Rx.Observable.interval(350), (id, interval) => {
+          return id
+        })
+
+        const trackAudioFeature = timeDelayedAlbumTrackId
+          .concatMap((id) => {
+            return Rx.Observable.fromPromise(spotifyApi.getAudioFeaturesForTrack(id)).pluck('body')
+          })
+
+        const albumTrackWithAudioFeature = Rx.Observable.zip(albumTrack, trackAudioFeature, (albumTrack, trackAudioAnalysis) => {
+          return {albumTrack, trackAudioAnalysis}
+        })
+
+        const albumWithFeatureAnalyzedTracks = Rx.Observable.zip(album, albumTrackWithAudioFeature.toArray(), (album, albumTracksWithAudioFeature) => {
+          return {album, albumTracksWithAudioFeature}
+        })
 
         return Rx.Observable.merge(artistAlbums)
       })
       .subscribe(x => console.log(x))
 
-    const combinedApiArtist = Rx.Observable.zip(spotifyApi, artistList)
+//    const combinedApiArtist = Rx.Observable.zip(spotifyApi, artistList)
 
     // combinedApiArtist.subscribe(x => console.log(x))
     // TODO
