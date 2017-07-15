8272365d623f86dfc902754dde70968bde0ef4e6
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-15 17:39:57 +0530

    Working on the adding support for multiple spotify clientIds

---
+++ spotify-client-id-secret.js
@@ -0,0 +1,11 @@
+/**
+ * Created by rprat on 15-07-2017.
+ */
+
+module.exports = {
+  clientDetails: [
+    {clientId: '34a4433b6ac746d385aab945f3216821', clientSecret: 'b295976aa0664157bb9e92ba4a184be0'},
+    {clientId: '774bb5437ad04881b6e754fb0ce9eece', clientSecret: '2cf32616de674e179c6d13168c20cdb5'},
+    {clientId: 'f22ae634bb5342a1906c6aa4708091d2', clientSecret: '8e54870487624a75a5d5ba5f87e46f34'}
+  ]
+}
