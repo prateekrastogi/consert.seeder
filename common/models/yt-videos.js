891045d33e44e381b48c7aec304c7d25e3f1092d
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-23 02:31:21 +0530

    Created test data for enriched topTracks

--- common/models/yt-videos.js
+++ common/models/yt-videos.js
@@ -16,16 +16,21 @@ module.exports = function (ytVideos) {
     }
 
     const artists = await enrichedArtists.find(filter)
+
     const tracks = _.flatMapDeep(artists, (artist) => {
       const track = _.flatMap(_.compact(artist.topTracks), track => track.track)
       return track
     })
-
-    const truncateTracks = _.map(_.compact(tracks), (track) => {
-      return track
+    const truncatedTracks = _.map(_.compact(tracks), ({name, popularity}) => {
+      return {name, popularity}
     })
 
-    const art = _.compact(_.filter(truncateTracks, (track) => track.popularity > 20 && track.popularity < 40))
+    const albums = _.flatMapDeep(artists, (artist) => artist.albums)
+    const truncatedAlbums = _.map(_.compact(albums), ({name, popularity}) => {
+      return {name, popularity}
+    })
+
+    const art = _.compact(_.filter(truncatedAlbums, (track) => track.popularity > 70 && track.popularity < 100))
 
     console.log(JSON.stringify(art, null, 1))
     // TODO
