4b65dd884102d01a6c23c3b07892bcff10529781
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-22 12:41:58 +0530

    added support for querying the enrichedartist dataset according to pre-defined parameters

--- common/models/yt-videos.js
+++ common/models/yt-videos.js
@@ -6,10 +6,18 @@ const _ = require('lodash')
 const loginAssist = require('../../lib/login-assist')
 
 module.exports = function (ytVideos) {
-  const enrichedArtists = app.models.enrichedArtists
   const youtube = loginAssist.ytLogin()
 
-  ytVideos.putArtistsLive = function (callback) {
+  ytVideos.putArtistsLive = async function (callback) {
+    const enrichedArtists = app.models.enrichedArtists
+
+    const filter = {
+      where: {and: [{or: [{isCrawled: false}, {isCrawled: {exists: false}}]}, {'artist.popularity': {'gte': 70}}, {'artist.popularity': {'lt': 100}}]},
+      fields: {artist: true}
+    }
+
+    const artists = await enrichedArtists.find(filter)
+    console.log(artists)
     // TODO
     callback(null)
   }
