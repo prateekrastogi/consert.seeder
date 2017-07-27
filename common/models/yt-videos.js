012e9c9959e30323a886c73ae910c4c162d5062c
Prateek Rastogi <p.rastogi@outlook.com>
2017-07-27 22:48:23 +0530

    adding keywords for classifier

--- common/models/yt-videos.js
+++ common/models/yt-videos.js
@@ -12,18 +12,20 @@ module.exports = function (ytVideos) {
   ytVideos.putArtistsLive = async function (callback) {
     const artists = await findArtistsByPopularity(70, 100)
     // putArtistsAlbumsLive(artists)
+/*
     classifier.train()
     searchYtVideos('metallica live', 200).subscribe(x => {
       const classy = classifier.getClassifications((x.snippet.title))
       console.log(classy)
       console.log(x.snippet.title)
     })
+*/
 
     /* ytVideos.pluck('id', 'videoId').bufferCount(10).concatMap((id) => {
      return getVideos(id.join())
      }).subscribe(x => console.log(JSON.stringify(x, null, 2)))*/
 
-//    getYtPlaylistItems('PLpUZ5waMZ6aMURwkm4_4927KTEpJHOT2S')
+   getYtPlaylistItems('PLE23710E948D187C0').subscribe(x => console.log(x))
     // TODO
     callback(null)
   }
