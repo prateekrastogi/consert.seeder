72a1910432cf519c0fed5ef17f9a852ef62abb11
Prateek Rastogi <p.rastogi@outook.com>
2018-01-25 17:28:30 +0530

    refactoring the code to use different tokens after quota hits

--- client-secrets.js
+++ client-secrets.js
@@ -3,9 +3,10 @@
  */
 
 module.exports = {
-  clientDetails: [
+  spotifyClientDetails: [
     {clientId: '34a4433b6ac746d385aab945f3216821', clientSecret: 'b295976aa0664157bb9e92ba4a184be0'},
     {clientId: '774bb5437ad04881b6e754fb0ce9eece', clientSecret: '2cf32616de674e179c6d13168c20cdb5'},
     {clientId: 'f22ae634bb5342a1906c6aa4708091d2', clientSecret: '8e54870487624a75a5d5ba5f87e46f34'}
-  ]
+  ],
+  ytAccessTokens: ['AIzaSyBnR6-mtXN6cMW8WgtzK1uvDOARpYDLbDg', 'AIzaSyBN43iX_-ci1212SWsZLtSjFnHW3SOsBNI']
 }
