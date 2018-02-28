2fba595d686ebe1bf20032bf310effe6a52417c6
Prateek Rastogi <p.rastogi@outook.com>
2018-02-28 16:22:11 +0530

    fixed bug of switching to different sets of tokens in case of live mode

--- common/models/yt-live.js
+++ common/models/yt-live.js
@@ -4,13 +4,17 @@ const R = require('ramda')
 const Rx = require('rxjs')
 const ytUtils = require('../../lib/yt-utils')
 
+const RETRY_COUNT = 3
+
 module.exports = function (ytLive) {
 /**
  * It syncs recently live Youtube music events with recombee & elasticsearch
  */
 
   ytLive.syncYtLiveEvents = function () {
-    const params = { type: `video`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
+    const params = { type: `video`, eventType: `live`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
+
+    ytUtils.searchYtVideos([`music`], 50, params).subscribe(x => console.log(x))
 
     return new Promise((resolve, reject) => resolve())
   }
