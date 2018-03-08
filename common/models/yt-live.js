d15b795bd7c05f57ef3c98ced2991e06d9db760a
Prateek Rastogi <p.rastogi@outook.com>
2018-03-08 13:02:57 +0530

    disable classifier filtering for live broadcasts searching

--- common/models/yt-live.js
+++ common/models/yt-live.js
@@ -6,6 +6,8 @@ const ytUtils = require('../../lib/yt-utils')
 const loginAssist = require('../../lib/login-assist')
 
 const RETRY_COUNT = 3
+const MAX_RESULTS = 300
+const MIN_CONCURRENT_VIEWERS = 50
 
 module.exports = function (ytLive) {
 /**
@@ -13,15 +15,28 @@ module.exports = function (ytLive) {
  */
 
   ytLive.syncYtLiveEvents = function () {
-    const params = { type: `video`, order: `viewCount`, topicId: `/m/04rlf`, eventType: `live`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
+    const params = { type: `video`, eventType: `live`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
 
-    const filters = {id: `UCl6ZTRWElpb5wE1zCgSDkGg`}
-    const channelsList = Rx.Observable.bindNodeCallback(loginAssist.ytLiveLogin().listChannels)
+    const viewCountMusicTopicParams = {...params}
+    const viewCountEntertainmentTopicParams = {...params, topicId: `/m/02jjt`}
 
-    channelsList(filters, [`statistics`, `topicDetails`], params).subscribe(x => console.log(x))
+    const viewCountMusicTopicSearch = ytUtils.searchYtVideos([`music | song | radio`], MAX_RESULTS, viewCountMusicTopicParams).retry(RETRY_COUNT)
+    const viewCountEntertainmentTopicSearch = ytUtils.searchYtVideos([`music | song | radio`], MAX_RESULTS, viewCountEntertainmentTopicParams).retry(RETRY_COUNT)
 
-    ytUtils.searchYtVideos([``], 200, params)
+    const viewCountAllSearch = Rx.Observable.merge(viewCountMusicTopicSearch)
+
+    viewCountAllSearch.filter(({liveStreamingDetails}) => {
+      const {concurrentViewers} = liveStreamingDetails
+      return parseInt(concurrentViewers) >= MIN_CONCURRENT_VIEWERS
+    })
+      .subscribe(x => console.log(x.snippet.title))
 
     return new Promise((resolve, reject) => resolve())
   }
 }
+
+/* const filters = {id: `UCl6ZTRWElpb5wE1zCgSDkGg`}
+    const channelsList = Rx.Observable.bindNodeCallback(loginAssist.ytLiveLogin().listChannels)
+
+    channelsList(filters, [`statistics`, `topicDetails`], params)
+ */
