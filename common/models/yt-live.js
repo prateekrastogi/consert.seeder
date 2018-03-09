312e8ad03305bcc162cb99a698d8b9cc80d68336
Prateek Rastogi <p.rastogi@outook.com>
2018-03-09 13:47:18 +0530

    added date, default, and viewcount based yt search of liveEvents

--- common/models/yt-live.js
+++ common/models/yt-live.js
@@ -8,7 +8,7 @@ const loginAssist = require('../../lib/login-assist')
 const RETRY_COUNT = 3
 const MAX_RESULTS = 300
 const THRESHOLD_CONCURRENT_VIEWERS = 50
-const THRESHOLD_CHANNEL_SUBSCRIBERS = 1000
+const THRESHOLD_CHANNEL_SUBSCRIBERS = 100
 const BUFFER_SIZE = 50
 
 module.exports = function (ytLive) {
@@ -19,41 +19,56 @@ module.exports = function (ytLive) {
   ytLive.syncYtLiveEvents = function () {
     const baseParams = { type: `video`, eventType: `live`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
 
-    const dateParams = {...baseParams, order: `date`}
-    const dateSearch = ytUtils.searchYtVideos([`music | song | radio -news -politics -sports`], MAX_RESULTS, dateParams).retry(RETRY_COUNT)
-
-    const tmep = dateSearch.bufferCount(BUFFER_SIZE).concatMap((bufferedBroadcasts) => {
-      const channelIdPlucker = R.compose(R.pluck('channelId'), R.pluck('snippet'))
-      const channelIds = R.compose(R.join(','), channelIdPlucker)(bufferedBroadcasts)
-
-      const listChannels = Rx.Observable.bindNodeCallback(loginAssist.ytLiveLogin().listChannels)
-
-      const channelStatistics = listChannels({id: `${channelIds}`}, [`statistics`], {}).retry(RETRY_COUNT).pluck('items')
-        .concatMap(channels => Rx.Observable.from(channels))
-
-      const eligibleBroadcastsObservable = channelStatistics.filter(({statistics}) => {
-        const {subscriberCount} = statistics
-        return parseInt(subscriberCount) >= THRESHOLD_CHANNEL_SUBSCRIBERS
-      }).bufferCount(BUFFER_SIZE)
-        .map((filteredChannels) => {
-          const filteredChannelsId = R.pluck('id')(filteredChannels)
-          const filter = R.compose(R.not, R.isNil, R.find(channelIdPlucker(R.__), filteredChannelsId))
-
-          const eligibleBroadcasts = R.filter(filter, bufferedBroadcasts)
-
-          return eligibleBroadcasts
-        })
-
-      return eligibleBroadcastsObservable
-    })
-
-    const allSearch = Rx.Observable.merge(tmep)
+    const allSearch = Rx.Observable.merge(dateSearch(baseParams))
 
     allSearch.do(x => console.log(x)).count()
       .subscribe(x => console.log(x))
 
     return new Promise((resolve, reject) => resolve())
   }
+}
+
+function searchLiveEvents () {
+  const baseParams = { type: `video`, eventType: `live`, regionCode: `US`, safeSearch: `none`, videoEmbeddable: `true`, videoSyndicated: `true` }
+
+  const dateSortedSearch = dateSearch(baseParams)
+  const defaultAndViewCountSortedSearch = defaultAndViewCountSearch(baseParams)
+
+  const allSearchResults = Rx.Observable.merge(dateSortedSearch, defaultAndViewCountSortedSearch)
+  return allSearchResults
+}
+
+function dateSearch (baseParams) {
+  const dateParams = {...baseParams, order: `date`}
+  const dateSearch = ytUtils.searchYtVideos([`music | song | radio -news -politics -sports`], MAX_RESULTS, dateParams).retry(RETRY_COUNT)
+
+  const dateSearchFilteredByChannelPopularity = dateSearch.bufferCount(BUFFER_SIZE).concatMap((bufferedBroadcasts) => {
+    const channelIds = R.compose(R.join(','), R.pluck('channelId'), R.pluck('snippet'))(bufferedBroadcasts)
+
+    const listChannels = Rx.Observable.bindNodeCallback(loginAssist.ytLiveLogin().listChannels)
+
+    const channelStatistics = listChannels({id: `${channelIds}`}, [`statistics`], {}).retry(RETRY_COUNT).pluck('items')
+      .concatMap(channels => Rx.Observable.from(channels))
+
+    const eligibleBroadcastsObservable = channelStatistics.filter(({statistics}) => {
+      const {subscriberCount} = statistics
+      return parseInt(subscriberCount) >= THRESHOLD_CHANNEL_SUBSCRIBERS
+    }).bufferCount(BUFFER_SIZE)
+      .map((filteredChannels) => {
+        const filteredChannelsId = R.pluck('id')(filteredChannels)
+
+        const eligibleBroadcasts = R.innerJoin(
+          (broadcast, channelId) => broadcast.snippet.channelId === channelId,
+          bufferedBroadcasts,
+          filteredChannelsId
+        )
+        return eligibleBroadcasts
+      }).concatMap(broadcasts => Rx.Observable.from(broadcasts))
+
+    return eligibleBroadcastsObservable
+  })
+
+  return dateSearchFilteredByChannelPopularity
 }
 
 function defaultAndViewCountSearch (baseParams) {
