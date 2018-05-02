a4355a23eacc943ca6ac0a8e3a3fa5c36bff9745
Prateek Rastogi <p.rastogi@outlook.com>
2018-05-02 23:05:05 +0530

    trying to reach  maximum api rate

--- common/models/artist-seed.js
+++ common/models/artist-seed.js
@@ -7,7 +7,7 @@ const Rx = require('rxjs')
 
 const RETRY_COUNT = 10
 const MAX_CONCURRENCY = 1
-const REQUEST_INTERVAL = 250
+const REQUEST_INTERVAL = 100
 
 module.exports = function (artistSeed) {
   artistSeed.putTopSpotifyArtists = async function () {
