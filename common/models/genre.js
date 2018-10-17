4fb9f570b2e2679fa4ea0ac98585b613b7f2eaba
Prateek Rastogi <prateek.rastogi@timesinternet.in>
2018-10-17 13:42:20 +0530

    removed reombee code deps :-)

--- common/models/genre.js
+++ common/models/genre.js
@@ -2,9 +2,10 @@
 
 const genresList = require('../../lib/genres')
 const _ = require('lodash')
-const Rx = require('rxjs-compat')
-const recombeeClient = require('../../lib/login-assist').recombeeLogin()
-const recombeeRqs = require('recombee-api-client').requests
+const { from, concat } = require('rxjs')
+const { concatMap } = require('rxjs/operators')
+
+const recommenderUtils = require('../../lib/recommender-utils')
 
 const terminateAllActiveInterferingSubscriptions = require('../../lib/misc-utils').terminateAllActiveInterferingSubscriptions
 
@@ -12,36 +13,39 @@ let activeSubscriptions = []
 
 module.exports = function (genre) {
   genre.seedGenreItemsToRecommender = function () {
+    let count = 0
+
     let genres = []
     _.forIn(Object.assign({}, genresList.genreTree), (value, key) => {
       genres = _.concat(genres, { key, value })
     })
 
-    const genreItemsSyncer = Rx.Observable.from(genres).concatMap(({ key, value }) => {
-      const genreItem = convertGenreToRecombeeGenreItem(value)
+    const genreItemsSyncer = from(genres).pipe(
+      concatMap(({ key, value }) => {
+        const recommenderItem = convertGenreToRecommenderGenreItem(value)
 
-      return Rx.Observable.fromPromise(recombeeClient.send(new recombeeRqs.SetItemValues(key, genreItem, { 'cascadeCreate': true })))
-    })
+        return recommenderUtils.writeBufferedItemsToRecommender([{ id: key, recommenderItem }], {})
+      })
+    )
 
-    const safeGenreItemsSyncer = Rx.Observable.concat(terminateAllActiveInterferingSubscriptions(activeSubscriptions),
-      genreItemsSyncer)
+    const safeGenreItemsSyncer = concat(terminateAllActiveInterferingSubscriptions(activeSubscriptions), genreItemsSyncer)
 
-    const seedGenreItemsToRecombeeSubscription = safeGenreItemsSyncer
-      .subscribe(x => console.log(x), err => console.log(err), () => console.log(`All genre items seeded`))
+    const genreSyncerSubscription = safeGenreItemsSyncer
+      .subscribe(x => console.log(`Total genre items seeded in this invocation: ${++count}`), err => console.log(err), () => console.log(`All genre items seeded`))
 
-    activeSubscriptions.push(seedGenreItemsToRecombeeSubscription)
+    activeSubscriptions.push(genreSyncerSubscription)
 
     return new Promise((resolve, reject) => resolve())
   }
 
-  function convertGenreToRecombeeGenreItem (genre) {
-    const recombeeGenre = {
+  function convertGenreToRecommenderGenreItem (genre) {
+    const recommenderGenre = {
       'itemType': 'genre',
       'genres': genre.leaves,
       'childrenItems': genre.children,
       'snippet-thumbnails': genre.thumbnails
     }
 
-    return recombeeGenre
+    return recommenderGenre
   }
 }
